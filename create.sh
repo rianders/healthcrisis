abecfbc0f4a87cdde8345493e0fb57f36565ec53
ricklon <rianders@docs.rutgers.edu>
2023-04-14 21:05:52 -0400

    initial files

---
+++ create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create JavaScript files
+touch sketch.js
+touch SceneManager.js
+touch Scene.js
+touch GameEntity.js
+touch GameObject.js
+touch Sprite.js
+touch BackgroundObject.js
+touch HealthStatusManager.js
+touch IntroScene.js
+touch DecisionScene.js
+touch EndScene.js
+
+# Create an assets folder
+mkdir assets
+
